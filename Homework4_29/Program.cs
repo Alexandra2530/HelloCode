2b718c5c5d3b773bdd1e69824756b06fea020505
Alexandra <asmamaeva@gmail.com>
2022-10-16 01:01:36 +0300

    add Homework4_29

--- Homework4_29/Program.cs
+++ Homework4_29/Program.cs
@@ -13,7 +13,7 @@ for (int i = 0; i < array.Length; i++)
 for (int i = 0; i < array.Length; i++)
 {
     Console.Write(array[i]);
-    if (i!=array.Length - 1)
+    if (i < array.Length - 1)
     {
         Console.Write(", ");
     }
