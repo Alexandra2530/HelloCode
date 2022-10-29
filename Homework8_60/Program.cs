28a52e00e16f46da33bcfd615b78560f774b9364
Alexandra <asmamaeva@gmail.com>
2022-10-29 04:32:24 +0300

    add Homework8_62

---
+++ Homework8_60/Program.cs
@@ -0,0 +1,86 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся
+// двузначных чисел. Напишите программу, которая будет построчно
+// выводить массив, добавляя индексы каждого элемента.
+
+int[,,] GetDemension()
+{
+    Console.WriteLine("Введите количество строк: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите глубину массива: ");
+    int l = Convert.ToInt32(Console.ReadLine());
+    int[,,] array = new int[m, n, l];
+    return array;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    for (int k = 0; k < array.GetLength(2); k++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j, k]} ({i}, {j}, {k})");
+        }
+        Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[,,] array)
+{
+    int count = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(1, 10);
+                array[i, j, k] = count;
+                count += 1;
+                if (count >= 100) break;
+            }
+            if  (count >= 100) break;
+        }
+        if  (count >= 100) break;
+    }
+}
+
+void CheckDoubleArray(int[,,] array)
+{
+    int count = 0;
+    int temp = array[count,count,count];
+    for (int i = 0; i < array.GetLength(0); i++);
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        if (count >= 100)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == temp)
+                {
+                    array[i, j, k] = new Random().Next(1, 10);
+                    count = 0;
+                    i = 0;
+                    j = 0;
+                    k = 0;
+                }
+            }
+            Console.WriteLine("Массив слишком большой");
+        }
+    }
+    count+=1;
+}
+
+int[,,] massiv = GetDemension();
+FillArray(massiv);
+PrintArray(massiv);
+CheckDoubleArray(massiv);
+PrintArray(massiv);
---
+++ Homework8_62/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int GetDemension(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[,] array, int n)
+{
+    int num = 1;
+    for (int delta = 0; delta < n - 2; delta++)
+    {
+        for (int i = 0 + delta; i < n - delta; i++)
+        {
+            array[0 + delta, i] = num;
+            num++;
+        }
+        num--;
+        for (int i = 0 + delta; i < n - delta; i++)
+        {
+            array[i, n - 1 - delta] = num;
+            num++;
+        }
+        num--;
+        for (int i = n - 1 - delta; i >= 0 + delta; i--)
+        {
+            array[n - 1 - delta, i] = num;
+            num++;
+        }
+        num--;
+        for (int i = n - 1 - delta; i >= 1 + delta; i--)
+        {
+            array[i, 0 + delta] = num;
+            num++;
+        }
+    }
+}
+
+int n = GetDemension("Введите количество строк/столбцов");
+int[,] massiv = new int[n, n];
+FillArray(massiv, n);
+PrintArray(massiv);
