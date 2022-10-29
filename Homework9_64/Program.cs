f6e83a56dd6d1edbe62259fccddf9b79745fdc71
Alexandra <asmamaeva@gmail.com>
2022-10-29 15:01:34 +0300

    add Homework9_64

---
+++ Homework9_64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+void Sequence(double number)
+{
+    if (number > 1)
+    {
+        Console.Write($"{number},");
+        Sequence(number - 1);
+    }
+    else
+    {
+        Console.Write(number);
+        Console.WriteLine();
+    }
+}
+
+double GetNumder()
+{
+    Console.WriteLine("Введите число N");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+Sequence(Math.Truncate(GetNumder()));
